99885fe4febef006672b82c8075628c168b4c13c
Emmanuele Levi <ignisvictorious@gmail.com>
2025-12-02 15:10:08 +0000

    Add query and removed for in favour of list comprehension

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #
 #  Import LIBRARIES
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 #  Import FILES
 from .data.post_db import BLOG_POST
@@ -17,8 +17,34 @@ def home() -> dict[str, str]:
 
 
 @app.get(path="/posts")
-def list_posts() -> dict[str, list[dict[str, int | str]]]:
+def list_posts(
+    query_parm: str | None = Query(default=None, description="Texto para buscar por título"),
+) -> dict[str, list[dict[str, int | str]] | str | None]:
+    if query_parm:
+        results: list[dict[str, int | str]] = [
+            post for post in BLOG_POST if query_parm.lower() in str(post["title"]).lower()
+        ]
+        return {"data": results, "query_parm": query_parm}
     return {"data": BLOG_POST}
+
+
+# @app.get(path="/posts")
+# def list_posts(
+#     query_parm: str | None = Query(default=None, description="Texto para buscar por título"),
+# ) -> dict[str, list[dict[str, int | str]] | str | None]:
+#     if query_parm:
+#         results: list[dict[str, int | str]] = []
+#         for post in BLOG_POST:
+#             if query_parm.lower() in str(post["title"]).lower():
+#                 results.append(post)
+#         print(f"results={results}")
+#         return {"data": results, "query_parm": query_parm}
+#     return {"data": BLOG_POST}
+
+
+# @app.get(path="/posts")
+# def list_posts() -> dict[str, list[dict[str, int | str]]]:
+#     return {"data": BLOG_POST}
 
 
 # app = FastAPI()
